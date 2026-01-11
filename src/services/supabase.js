3a5e721c81a3663c763b4830cbabd82ca0dffae1
Arthur Alves <arthurgostoso78@gmail.com>
2026-01-11 15:53:45 -0300

    Ajustes basicos

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Cria a conexão
+export const supabase = createClient(supabaseUrl, supabaseKey);
